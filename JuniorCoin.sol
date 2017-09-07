c8fbedf917a2fc3703673f05839caa122e79a529
JuniorCode <JuniorCode@users.noreply.github.com>
2017-09-07 16:29:35 -0400

    Update JuniorCoin.sol

--- JuniorCoin.sol
+++ JuniorCoin.sol
@@ -1,23 +1,46 @@
 pragma solidity ^0.4.11;
 
 import './IERC20.sol';
+import './SafeMath.sol';
 
 contract JuniorCoin is IERC20 {
     
-    uint public constant _totalSupply = 21000000;
+    using SafeMath for uint256;
+    
+    uint public _totalSupply = 21000000;
     
     string public constant symbol = "XJR";
     string public constant name = "JuniorCoin";
 
-    uint8 public constant decimals = 4;
+    uint8 public constant decimals = 18;
+    
+    // 1 ETHER = 100 XJR
+    uint256 public constant RATE = 100;
+    
+    address public owner;
 
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
+    
+    function () payable {
+        createTokens();
+    }
 
     event Transfer(address indexed from, address indexed to, uint256 value);
 
     function JuniorCoin() {
+        owner = msg.sender;
         balances[msg.sender] = _totalSupply;
+    }
+    
+    function createTokens() payable {
+        require(msg.value > 0);
+        
+        uint256 tokens = msg.value.mul(RATE);
+        balances[msg.sender] = balances[msg.sender].add(tokens);
+        _totalSupply = _totalSupply.add(tokens);
+        
+        owner.transfer(msg.value);
     }
     
     function totalSupply() constant returns (uint256 totalSupply) {
@@ -33,8 +56,8 @@ contract JuniorCoin is IERC20 {
             balances[msg.sender] >= _value
             && _value > 0
         );
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
         Transfer(msg.sender, _to, _value);
         return true;
     }
@@ -45,9 +68,9 @@ contract JuniorCoin is IERC20 {
             && balances[_from] >= _value
             && _value > 0
         );
-        balances[_from] -= _value;
-        balances[_to] += _value;
-        allowed[_from][msg.sender] -= _value;
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
         Transfer(_from, _to, _value);
         return true;
     }
